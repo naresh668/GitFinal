df402d02f57624e4d87fb020fc12cba032cf94c3
naresh <nareshreddylgm@gmail.com>
2021-06-03 15:22:26 +0530

    naresh got it

--- src/main/java/com/order/controller/OrderController.java
+++ src/main/java/com/order/controller/OrderController.java
@@ -22,7 +22,7 @@ public class OrderController {
 
 	@PostMapping("/bookOrder")
 	public TranscationResponse bookOrder(@RequestBody TranscationRequest request) {
-
+String re="null";
 
 		return orderService.saveOrder(request);
 
